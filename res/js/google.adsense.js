3429ff234525a4b4b940d21c386575b8aba770c8
mingz <305603665@qq.com>
2018-11-02 12:04:10 +0800

    update web site

---
+++ res/js/google.adsense.js
@@ -0,0 +1,18 @@
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+    }
+    else {
+        bp.src = 'http://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+
+    (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-8794583546569613",
+    enable_page_level_ads: true
+  });
+
+})();
